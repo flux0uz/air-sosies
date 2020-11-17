bf431fa5660ffbb8641961cdd2b8b3e5eadb23ef
william bailey <williambaileypro@gmail.com>
2020-11-17 13:04:39 +0100

    booking controller done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,23 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_info)
+    @booking.save
+    redirect_to dashboard_path
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def booking_info
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :double_id, :total_price)
   end
 end
