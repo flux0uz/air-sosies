34f62e027aa2addf12ae87d5afeba668376b4211
Guillaume Potier <guillaume_potier@live.fr>
2020-11-17 12:58:20 +0100

    Added image_URL column to Doubles table and an image_URL in the seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_140551) do
+ActiveRecord::Schema.define(version: 2020_11_17_113924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_140551) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_URL"
     t.index ["user_id"], name: "index_doubles_on_user_id"
   end
 
